f50fb53cfaeda8520a32ac7ef176edacd7691123
YO-LO-vv <919440676@qq.com>
2022-08-23 19:17:41 +0800

    add element plus

---
+++ src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button>woce</el-button>
+    <el-button type="success">Success</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cenima'
+}
+</script>
+
+
+<style>
+
+</style>
