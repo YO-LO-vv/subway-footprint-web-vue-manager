25bda10882713e72bb84719757baeae3848ca374
YO-LO-vv <919440676@qq.com>
2022-08-24 15:40:54 +0800

    top menu frame

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import {createRouter,createWebHashHistory} from "vue-router"
 
 const routes=[
-    {
+    {   //登陆界面
         path:'/',
         name:'login',
         component:()=>import(/*webpackChunckName:'Login*/ '@/page/login.vue')
+    },
+
+    {   //主界面
+        path:'/index',
+        name:'index',
+        component:()=>import(/*webpackChunckName:'Index*/ '@/page/index.vue')
     },
 ]
 
